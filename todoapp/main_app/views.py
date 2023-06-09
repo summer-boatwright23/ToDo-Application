16097de5f9a028af5f265ee573f4693c579402d5
summer-boatwright <summerrane14@gmail.com>
2023-06-09 18:25:59 -0400

    imports CreateView

--- todoapp/main_app/views.py
+++ todoapp/main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-# from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 # Create your views here.
