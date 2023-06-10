6fd806bbf0392a0774d50088539b2d5499f38e48
summer-boatwright <summerrane14@gmail.com>
2023-06-10 03:24:19 -0400

    creates DeleteView class

--- todoapp/main_app/views.py
+++ todoapp/main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy
 
@@ -25,3 +25,6 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+    
+class DeleteView(DeleteView):
+    model =
