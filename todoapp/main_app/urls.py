d61831e0c84c3e06a5cc86e74cd60f13d5ce797b
summer-boatwright <summerrane14@gmail.com>
2023-06-10 03:40:25 -0400

    imports deleteview into urls.py

--- todoapp/main_app/urls.py
+++ todoapp/main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
