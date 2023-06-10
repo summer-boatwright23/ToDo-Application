b48524e77326f9302d8c1369860c26429b1c342f
summer-boatwright <summerrane14@gmail.com>
2023-06-10 03:49:33 -0400

    finalizes delete functionality

--- todoapp/main_app/urls.py
+++ todoapp/main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update')
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete')
 ]
